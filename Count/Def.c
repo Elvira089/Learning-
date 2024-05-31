699510f858283358a526cad69e85629bb141e9ff
Elvira089 <162065954+Elvira089@users.noreply.github.com>
2024-05-31 03:00:20 +0100

    Def.c
    Largest number in an array

--- Count/Def.c
+++ Count/Def.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  int array[n];
+  int i;
+  int largest number;
+printf("Enter the length of the array:\n");
+scanf("%d",&n);
+printf("Enter the elements of an array:\n");
+for( i=0 ; i<n ; i++){
+scanf("%d",&array[i]);
+printf("The largest number in the array is %d",largest number);
+return 1;
+}
+
+
 
